9d6d5f7f7a43764a5caa2266b3451730d3055973
juanmgallego96 <97921349+juanmgallego96@users.noreply.github.com>
2024-06-19 16:06:46 -0500

    AA

--- mysite/views.py
+++ mysite/views.py
@@ -3,4 +3,36 @@ from decouple import config
 
 def index(request):
     string=config('VAR')
+    string=get_secret()
     return HttpResponse(string)
+
+
+import boto3
+from botocore.exceptions import ClientError
+
+
+def get_secret():
+
+    secret_name = "env/aws-test"
+    region_name = "us-east-2"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    secret = get_secret_value_response['SecretString']
+    print(secret)
+    return secret
+    # Your code goes here.
