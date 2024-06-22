bbdd87b48823908a4be03c1623fe4d5f65f1871c
juanmgallego96 <97921349+juanmgallego96@users.noreply.github.com>
2024-06-22 11:00:58 -0500

    Edit views

--- mysite/views.py
+++ mysite/views.py
@@ -1,45 +1,14 @@
 from django.http import HttpResponse
-from decouple import config
+# from decouple import config
 import os
-from os.path import join, dirname
-from dotenv import load_dotenv
+# from os.path import join, dirname
+# from dotenv import load_dotenv
 
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
+# dotenv_path = join(dirname(__file__), '.env')
+# load_dotenv(dotenv_path)
 
-VAR = os.environ.get("VAR")
+VAR = os.getenv("VAR")
 
 def index(request):
 
     return HttpResponse(VAR)
-
-
-# import boto3
-# from botocore.exceptions import ClientError
-
-
-# def get_secret():
-
-#     secret_name = "env/aws-test"
-#     region_name = "us-east-2"
-
-#     # Create a Secrets Manager client
-#     session = boto3.session.Session()
-#     client = session.client(
-#         service_name='secretsmanager',
-#         region_name=region_name
-#     )
-
-#     try:
-#         get_secret_value_response = client.get_secret_value(
-#             SecretId=secret_name
-#         )
-#     except ClientError as e:
-#         # For a list of exceptions thrown, see
-#         # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-#         raise e
-
-#     secret = get_secret_value_response['SecretString']
-#     print(secret)
-#     return secret
-    # Your code goes here.
